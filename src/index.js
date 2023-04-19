9ba5c98ba095f93eceac58dac78df4da1a6d1416
FEDERICO SZNAJDERHAUS <47206175@ort>
2023-04-19 09:11:44 -0300

    funca en postman

--- src/index.js
+++ src/index.js
@@ -3,20 +3,35 @@ import Pizza from './models/pizza.js';
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 3001;
+app.use(express.json());
 
 app.get ('/:id', async(req, res)=>{
     const id = await getById(req.params.id);
     res.send(id);
 })
+
 app.get ('/', async(req, res)=>{
     const pizzas = await getAll();
     res.send(pizzas);
 })
+
 app.delete ('/:id', async(req, res)=>{
     const idBorrado = await deleteById(req.params.id);
     res.send(idBorrado);
 })
+
+app.put ('/api/:id', async(req, res)=>{
+    const id = req.params.id;
+    const pizza= new pizza();
+    pizza.Nombre=req.body.Nombre;
+    pizza.LibreGluten=req.body.LibreGluten;
+    pizza.Importe = req.body.Importe;
+    pizza.Descripcion=req.body.Descripcion;
+    const cambiado = await update(pizza, id);
+    res.send(cambiado);
+})
+
 app.listen (port, ()=>{
     console.log(`EJEMPLO ${port}`)
 })
--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,7 @@
 const config = {
     user: 'Pizzas',
     password: 'Pizzas',
-    server: 'A-PHZ2-CEO-014',
+    server: 'A-PHZ2-AMI-012',
     database: 'DAI-Pizzas',
     options: {
         trustServerCertificate: true,
