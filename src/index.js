31eabe461dc8d3c5d427e425b2f9437f9eba3da8
FEDERICO SZNAJDERHAUS <47206175@ort>
2023-04-21 10:42:34 -0300

    En teoría terminado

--- src/index.js
+++ src/index.js
@@ -7,29 +7,38 @@ const port = 3001;
 app.use(express.json());
 
 app.get ('/:id', async(req, res)=>{
-    const id = await getById(req.params.id);
-    res.send(id);
+    const id            = await getById(req.params.id);
+    res.status(200).send(id);
 })
 
 app.get ('/', async(req, res)=>{
-    const pizzas = await getAll();
-    res.send(pizzas);
+    const pizzas        = await getAll();
+    res.status(200).send(pizzas);
 })
 
 app.delete ('/:id', async(req, res)=>{
-    const idBorrado = await deleteById(req.params.id);
-    res.send(idBorrado);
+    const idBorrado     = await deleteById(req.params.id);
+    res.status(200).send(idBorrado);
 })
 
-app.put ('/api/:id', async(req, res)=>{
-    const id = req.params.id;
-    const pizza= new pizza();
-    pizza.Nombre=req.body.Nombre;
-    pizza.LibreGluten=req.body.LibreGluten;
-    pizza.Importe = req.body.Importe;
-    pizza.Descripcion=req.body.Descripcion;
-    const cambiado = await update(pizza, id);
-    res.send(cambiado);
+app.put ('/:id', async(req, res)=>{
+    const id            = req.params.id;
+    const pizza         = new Pizza();
+    pizza.Nombre        = req.body.Nombre;
+    pizza.LibreGluten   = req.body.LibreGluten;
+    pizza.Importe       = req.body.Importe;
+    pizza.Descripcion   = req.body.Descripcion;
+    const cambiado      = await update(pizza, id);
+    res.status(200).send(cambiado);
+})
+app.post('/', async(req, res)=>{
+    const pizza         = new Pizza();
+    pizza.Nombre        = req.body.Nombre;
+    pizza.LibreGluten   = req.body.LibreGluten;
+    pizza.Importe       = req.body.Importe;
+    pizza.Descripcion   = req.body.Descripcion;
+    const cambiado      = await create(pizza);
+    res.status(200).send(cambiado);
 })
 
 app.listen (port, ()=>{
